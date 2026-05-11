ea6af298c9b6a4b70c90aa6e2954ec7f60ad894f
PatrykMargasinski <49985857+PatrykMargasinski@users.noreply.github.com>
2026-05-11 23:08:38 +0200

    Added inputs

--- soclover.client/src/components/board/Board.tsx
+++ soclover.client/src/components/board/Board.tsx
@@ -6,19 +6,41 @@ import { useContext } from "react";
 export default function Board() {
     const boardContext = useContext(BoardContext);
 
-    if (!boardContext)
+    if (!boardContext || !boardContext.board)
         return <div>Board loading...</div>;
 
     const { board } = boardContext;
 
     return (
-        <div className={styles.boardWrapper}>
+        <div className={styles.boardContainer}>
+            <div className={styles.inputRow}>
+                <input
+                    className={`${styles.clueInput} ${styles.top}`}
+                />
+            </div>
+
             <div className={styles.middleRow}>
+                <input
+                    className={`${styles.clueInput} ${styles.left}`}
+                />
+
                 <div className={styles.grid}>
-                    {board?.boardSlots.sort((a, b) => a.positionIndex - b.positionIndex).map((slot) => (
-                        <BoardSlot key={slot.positionIndex} slot={slot} />
-                    ))}
+                    {board.boardSlots
+                        .sort((a, b) => a.positionIndex - b.positionIndex)
+                        .map((slot) => (
+                            <BoardSlot key={slot.positionIndex} slot={slot} />
+                        ))}
                 </div>
+
+                <input
+                    className={`${styles.clueInput} ${styles.right}`}
+                />
+            </div>
+
+            <div className={styles.inputRow}>
+                <input
+                    className={`${styles.clueInput} ${styles.bottom}`}
+                />
             </div>
         </div>
     );
