7e9ef9c925af949c48fe6fa116fa0cbba678ce3f
PatrykMargasinski <49985857+PatrykMargasinski@users.noreply.github.com>
2026-05-18 18:56:20 +0200

    Database optimalization

--- SoClover.Server/Context/SoCloverDBContext.cs
+++ SoClover.Server/Context/SoCloverDBContext.cs
@@ -44,27 +44,29 @@ namespace SoClover.Server.Context
                 .HasMany(b => b.BoardSlots)
                 .WithOne(s => s.Board)
                 .HasForeignKey(s => s.BoardId)
-                .OnDelete(DeleteBehavior.Cascade);
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<BoardSlot>()
                 .HasOne(s => s.GameRoomCard)
-                .WithOne()
+                .WithOne(grc => grc.BoardSlot)
                 .HasForeignKey<BoardSlot>(s => s.GameRoomCardId)
                 .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<BoardSlot>()
                 .HasOne(s => s.TargetGameRoomCard)
-                .WithMany(grc => grc.TargetBoardSlots)
-                .HasForeignKey(s => s.TargetGameRoomCardId)
+                .WithOne(grc => grc.TargetBoardSlot)
+                .HasForeignKey<BoardSlot>(s => s.TargetGameRoomCardId)
                 .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<GameRoom>()
-                .Property(g => g.RoomCode);
 
             modelBuilder.Entity<GameRoomCard>().HasOne(grc => grc.Card)
                 .WithMany(c => c.GameRoomCards) 
                 .HasForeignKey(grc => grc.CardId)
                 .OnDelete(DeleteBehavior.Restrict);
+
+            //indexes
+            modelBuilder.Entity<GameRoom>().HasIndex(g => g.RoomCode) .IsUnique();
+            modelBuilder.Entity<Player>().HasIndex(p => p.PlayerGuid).IsUnique();
+            modelBuilder.Entity<Player>().HasIndex(p => p.ConnectionId);
 
             CardSeeder.Seed(modelBuilder);
         }
---
+++ SoClover.Server/Migrations/20260518164646_RemovedShadowPropertyAndAddedIndexes.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoClover.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedShadowPropertyAndAddedIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BoardSlots_Boards_BoardId",
+                table: "BoardSlots");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BoardSlots_GameRoomCards_GameRoomCardId1",
+                table: "BoardSlots");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BoardSlots_GameRoomCardId1",
+                table: "BoardSlots");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BoardSlots_TargetGameRoomCardId",
+                table: "BoardSlots");
+
+            migrationBuilder.DropColumn(
+                name: "GameRoomCardId1",
+                table: "BoardSlots");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_ConnectionId",
+                table: "Players",
+                column: "ConnectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_PlayerGuid",
+                table: "Players",
+                column: "PlayerGuid",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameRooms_RoomCode",
+                table: "GameRooms",
+                column: "RoomCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardSlots_TargetGameRoomCardId",
+                table: "BoardSlots",
+                column: "TargetGameRoomCardId",
+                unique: true,
+                filter: "[TargetGameRoomCardId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BoardSlots_Boards_BoardId",
+                table: "BoardSlots",
+                column: "BoardId",
+                principalTable: "Boards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BoardSlots_Boards_BoardId",
+                table: "BoardSlots");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_ConnectionId",
+                table: "Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_PlayerGuid",
+                table: "Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GameRooms_RoomCode",
+                table: "GameRooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BoardSlots_TargetGameRoomCardId",
+                table: "BoardSlots");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GameRoomCardId1",
+                table: "BoardSlots",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardSlots_GameRoomCardId1",
+                table: "BoardSlots",
+                column: "GameRoomCardId1",
+                unique: true,
+                filter: "[GameRoomCardId1] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardSlots_TargetGameRoomCardId",
+                table: "BoardSlots",
+                column: "TargetGameRoomCardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BoardSlots_Boards_BoardId",
+                table: "BoardSlots",
+                column: "BoardId",
+                principalTable: "Boards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BoardSlots_GameRoomCards_GameRoomCardId1",
+                table: "BoardSlots",
+                column: "GameRoomCardId1",
+                principalTable: "GameRoomCards",
+                principalColumn: "Id");
+        }
+    }
+}
